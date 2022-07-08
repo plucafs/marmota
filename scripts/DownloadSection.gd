6c1d4670303e36fe5f0b6b898ca052ab199a5dc3
plucafs <l.poli@pm.me>
2022-07-08 12:15:14 +0200

    Open folder after download
    - Setup for file extension selection

--- scripts/DownloadSection.gd
+++ scripts/DownloadSection.gd
@@ -1,25 +1,49 @@
 extends Control
 
 onready var format_selector = get_node("DownloadSection/OtherPar/FormatSelector")
+onready var audio_format_selector = get_node("DownloadSection/OtherPar/AudioFormatSelector")
 
 func _ready():
 	format_selector.add_item("Video + audio")
 	format_selector.add_item("Audio only")
-
+	audio_format_selector.add_item("aac")
+	audio_format_selector.add_item("flac")
+	audio_format_selector.add_item("mp3")
+	audio_format_selector.add_item("m4a")
+	audio_format_selector.add_item("opus")
+	audio_format_selector.add_item("vorbis")
+	audio_format_selector.add_item("wav")
+	audio_format_selector.add_item("alac")
+	
 func download_video():
-	var yt_dl_path = Global.yt_dl_dir
-	var link = " " + $DownloadSection/VideoLinkContainer/VideoLinkLineEdit.get_text()
-#		Don't insert a space (" ") before the -P
-	var download_directory = "-P " + $DownloadSection/DownloadPathContainer/DownloadPathLineEdit.get_text()
+	var yt_dl_path = Global.yt_dl_dir + " "
 	var format = ""
+	var audio_format = ""
+	var download_directory = "-P " + $DownloadSection/DownloadPathContainer/DownloadPathLineEdit.get_text() + " "
+	var link = $DownloadSection/VideoLinkContainer/VideoLinkLineEdit.get_text()
 	
 	if format_selector.selected == 0:
 		format = ""
-	if format_selector.selected == 1:
-#		Don't insert a space (" ") before the -x
-		format = "-x" 
-		
-	$DownloadSection/OtherPar/CompleteCommand.text = str(yt_dl_path) + format + download_directory + link
+	elif format_selector.selected == 1:
+		format = "-x " 
+		if audio_format_selector.selected == 0:
+			audio_format = "--audio-format aac "
+		elif audio_format_selector.selected == 1:
+			audio_format = "--audio-format flac"
+		elif audio_format_selector.selected == 2:
+			audio_format = "--audio-format mp3"
+		elif audio_format_selector.selected == 3:
+			audio_format = "--audio-format m4a"
+		elif audio_format_selector.selected == 4:
+			audio_format = "--audio-format opus"
+		elif audio_format_selector.selected == 5:
+			audio_format = "--audio-format vorbis"
+		elif audio_format_selector.selected == 6:
+			audio_format = "--audio-format wav"
+		elif audio_format_selector.selected == 7:
+			audio_format = "--audio-format alac"
+	
+	$DownloadSection/OtherPar/CompleteCommand.text = yt_dl_path + format + audio_format + download_directory + link
 	
 	if $DownloadSection/ytdlpPath/ytdlpPathLineEdit.text == "":
 		$DownloadSection/OtherPar/Debug.text = "Select the correct path to yt-dlp.exe"
@@ -28,6 +52,7 @@ func download_video():
 			$DownloadSection/OtherPar/Debug.text = "Please, add one URL!"
 		else:
 			OS.execute(yt_dl_path, [format, download_directory, link])
+			OS.shell_open($DownloadSection/DownloadPathContainer/DownloadPathLineEdit.get_text())
 
 func _on_VideoLinkButton_pressed():
 	$DownloadSection/VideoLinkContainer/VideoLinkLineEdit.text = OS.clipboard
